d9447804f7f6c02e2d970311f9c5da4507486e48
Zeke Sikelianos <zeke@sikelianos.com>
2018-11-12 21:05:01 -0800

    add state to results, and unignore allRows.csv final result

--- find-county.js
+++ find-county.js
@@ -1,4 +1,15 @@
+const got = require('got')
 // const counties = require('us-counties')
-const counties = require('./nm-and-az-counties.json')
-const whichPolygon = require('which-polygon')
-module.exports = whichPolygon(counties)
+// const counties = require('./nm-and-az-counties.json')
+// const whichPolygon = require('which-polygon')
+// module.exports = whichPolygon(counties)
+
+module.exports = async function findCounty (lng, lat) {
+  const url = `http://localhost:5000/?lng=${lng}&lat=${lat}`
+  const { body } = await got(url, {json: true})
+  // console.log(body)
+  return {
+    county: body.NAME10,
+    state: body.state
+  }
+}
--- index.js
+++ index.js
@@ -14,55 +14,53 @@ const desiredFields = [
   'ACRES'
 ]
 
-const stateCodes = {
-  '35': 'NM',
-  '04': 'AZ'
-}
-
 const fields = []
 let allRows = []
 
-// collection all rows from all files
-walk(dataDir)
-  .filter(file => file.relativePath.endsWith('.json'))
-  .filter(file => file.relativePath.includes('-ids'))
-  .forEach(file => {
-    const fullPath = path.join(dataDir, file.relativePath)
-    console.log(file.relativePath)
-    const rows = getRowsFromFile(fullPath)
-    fields.push(Object.keys(rows[0]))
-    allRows = allRows.concat(rows)
+main()
+
+async function main() {
+
+  // collection all rows from all files
+  walk(dataDir)
+    .filter(file => file.relativePath.endsWith('.json'))
+    .filter(file => file.relativePath.includes('-ids'))
+    .forEach(file => {
+      const fullPath = path.join(dataDir, file.relativePath)
+      console.log(file.relativePath.match(/(\d+)/)[1]) // print year of filename
+      const rows = getRowsFromFile(fullPath)
+      fields.push(Object.keys(rows[0]))
+      allRows = allRows.concat(rows)
+    })
+
+  console.log('find fields that are present in every dataset')
+  const commonFields = intersection(...fields)
+
+  commonFields.push('county', 'state')
+  fs.writeFileSync(path.join(__dirname, 'fields.json'), JSON.stringify(commonFields, null, 2))
+
+  console.log('clean up each row so it only includes the common fields')
+  for (let row of allRows) {
+    row = pick(row, commonFields)
+    const county = await findCounty(row.lng, row.lat)
+    if (county) {
+      Object.assign(row, county)
+      console.log(row.county, row.state)
+    }
+  }
+
+  const jsonFile = path.join(__dirname, 'allRows.json')
+  const outStream = fs.createWriteStream(jsonFile) 
+  console.log(`write ${allRows.length} rows to ${jsonFile}`)
+
+  const stringifyStream = bigJSON.createStringifyStream({body: allRows})
+
+  stringifyStream.on('end', () => {
+    console.log('done')
   })
 
-console.log('find fields that are present in every dataset')
-const commonFields = intersection(...fields)
-
-commonFields.push('county', 'state')
-fs.writeFileSync(path.join(__dirname, 'fields.json'), JSON.stringify(commonFields, null, 2))
-
-console.log('clean up each row so it only includes the common fields')
-allRows = allRows.map(row => { 
-  const result = pick(row, commonFields)
-  const county = findCounty([result.lng, result.lat])
-  if (county && county.NAME10) {
-    result.county = county.NAME10
-    result.state = stateCodes[county.STATEFP10]
-    console.log(result.county, result.state)
-  }
-  return result
-})
-
-const jsonFile = path.join(__dirname, 'allRows.json')
-const outStream = fs.createWriteStream(jsonFile) 
-console.log(`write ${allRows.length} rows to ${jsonFile}`)
-
-const stringifyStream = bigJSON.createStringifyStream({body: allRows})
-
-stringifyStream.on('end', () => {
-  console.log('done')
-})
-
-stringifyStream.pipe(outStream)
+  stringifyStream.pipe(outStream)
+}
 
 function getRowsFromFile (fullPath) {
   const data = require(fullPath)
@@ -82,3 +80,4 @@ function getRowsFromFile (fullPath) {
 
   return rows
 }
+
